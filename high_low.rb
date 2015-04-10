3a7ddf3fca0cd1f75f5a2ba489df3f656e25a6da
Ryan March <rmarch03@villanova.edu>
2015-04-09 20:49:20 -0400

    allowing for fluid, continuous play

--- high_low.rb
+++ high_low.rb
@@ -20,15 +20,40 @@ def play_round(guesses)
 	return false
 end
 
-puts "Please enter a lower bound: "
-$low = gets.to_i
-puts "Please enter an upper bound: "
-$high = gets.to_i
-puts "Please enter number of guesses you want to take: "
-guesses = gets.to_i
-
-if play_round(guesses)
-	puts "You got the number"
-else
-	puts "Better luck next time"
+def determine_if_player_will_continue
+	puts "Enter yes/y/1 to continue"
+	user_choice = gets.chomp
+	return user_choice == 'y' || user_choice == 'yes' || user_choice == '1'
 end
+
+puts ""
+puts ">" * 10
+puts "Welcome to the High Low Guessing Game!"
+puts "The computer will choose a random number between the bounds you specify"
+puts "You can enter how many guesses you want"
+puts "You will be told whether your guess is too high or too low"
+puts ""
+
+continue_playing = true
+
+while continue_playing
+	puts "Please enter a lower bound: "
+	$low = gets.to_i
+	puts "Please enter an upper bound: "
+	$high = gets.to_i
+	puts "Please enter number of guesses you want to take: "
+	guesses = gets.to_i
+
+	if play_round(guesses)
+		puts "You got the number"
+	else
+		puts "Better luck next time"
+	end
+
+	continue_playing = determine_if_player_will_continue()
+end
+
+puts "Thanks for playing!"
+puts "Exiting..."
+puts "<" * 10
+puts ""
